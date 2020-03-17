1e5356b2e428408e714e26f9d9bcd4d97af4c500
ReubenCa <reuben.carolan@gmail.com>
2020-03-17 22:55:31 +0000

    Completely restarted with altered
    Prior to this I have made a decent basic calculator
    that is more of a proof of concept, this is my start to the actual differentiator

--- Differentiator/Node.cs
+++ Differentiator/Node.cs
@@ -8,74 +8,16 @@ namespace Differentiator
 {
     class Node
     {
-      char Symbol;
-      protected  string[] Sections;
-        Object[] Children;
-      protected  List<string> Ints = new List<string>();
-       protected List<string> NonInts = new List<string>();
-        protected List<string> Vals = new List<string>();
-
-        public Node(string Equation)
+       protected string equation;
+       public Node(string equation_)
         {
-
-            Symbol = this.GetSymbol();
-
-            Console.WriteLine("Creating Node with contents: " + Equation);
-            Sections = Equation.Split(Symbol);
-            Children = new object[Sections.Length];
-            for (int i = 0; i < Sections.Length; i++)
-            {
-                Children[i] = new Equation(Sections[i]);
-            }
-        }
-        public  string GetValue()
-        {
-            for (int i = 0; i < Children.Length; i++)
-            {
-                Object obj = Children[i];
-                Equation eq = (Equation)obj;
-                string eqval = eq.Getvalue();
-                Vals.Add(eqval);
-            }
-            return CalculateVal();
+            equation = equation_;
+            ConsoleMessage(equation);
         }
 
-        public virtual string CalculateVal()
+        protected virtual void ConsoleMessage(string eq)
         {
-            Console.WriteLine("Error calling function CalculateVal");
-            return "Error";
-        }
-
-
-        public void SeperateIntoLists()
-        {
-            
-
-            for (int i = 0; i <Vals.Count(); i++)
-            {
-                string CurrentValueBeingSorted = Vals[i];
-                if (Isint(CurrentValueBeingSorted))
-                {
-                    Ints.Add(CurrentValueBeingSorted);
-                }
-                else
-                {
-                    NonInts.Add(CurrentValueBeingSorted);
-                }
-            }
-            
-        }
-
-
-        protected bool Isint(string a)
-        {
-            int x;
-            return int.TryParse(a, out x);
-           
-        }
-        public virtual char GetSymbol()
-        {
-            return '@';
+            Console.WriteLine("Created Node with contents {0}", eq);
         }
 
     }
---
+++ Differentiator/Section.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Differentiator
+{
+    class Section
+    {
+        string equation;
+        Node Child;
+       public Section(string equation_)
+        {
+            Console.WriteLine("Created Section with contents {0}", equation_);
+            equation = equation_;
+            bool bracketsremoved = false;
+            do
+            {
+                bracketsremoved = false;
+                
+                if (equation[0] == '(' && equation[equation.Length - 1] == ')')
+                {
+                    equation = equation.Substring(1, equation.Length - 2);
+                    bracketsremoved = true;
+                    Console.WriteLine("trimmed section to {0}", equation);
+                    
+                }
+
+            } while (bracketsremoved);
+
+            if (CheckIfCanSplitToSections())
+            {
+                Child = new SectionSplitterNode(equation);
+            }
+            else
+            {
+                DetermineRuleNeeded();
+            }
+        }
+
+
+        private void DetermineRuleNeeded()
+        {
+        }
+
+       private bool CheckIfCanSplitToSections()
+        {
+            int bracketcount = 0;
+            for(int i = 0;i<equation.Length-1;i++)
+            {
+                switch (equation[i])
+                {
+                    case '(':
+                        bracketcount++;
+                        break;
+                    case ')':
+                        bracketcount--;
+                        break;
+                    case '+':
+                        if (bracketcount == 0)
+                        {
+                            return true;
+                        }
+                        break;
+                    case '-':
+                        if (bracketcount == 0)
+                        {
+                            return true;
+                        }
+                        break;
+                }
+            }
+
+
+            return false;
+        }
+
+    }
+}
