425b5a57522b00de2c3a4a3e9fe51b154a2362af
ReubenCa <61869273+ReubenCa@users.noreply.github.com>
2020-03-18 10:00:52 +0000

    Started to Add Differentiation rules

---
+++ Differentiator/ExponentRule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Differentiator
+{
+    class ExponentRule : Node
+    {
+
+        public ExponentRule(string equation_) : base(equation_)
+        {
+
+        }
+    }
+}
--- Differentiator/Node.cs
+++ Differentiator/Node.cs
@@ -19,6 +19,32 @@ namespace Differentiator
         {
             Console.WriteLine("Created Node with contents {0}", eq);
         }
+        public virtual string Differentiate()
+        {
+            return "Node with contents " +  equation  + " did not contain a definition of how to differentiate";
+        }
 
+      protected int FindFirstCharacterIndex(char Character)
+        {
+            //ignores things in brackets
+            int bracketcount = 0;
+           
+            for (int i=0; i<equation.Length;i++)
+            {
+                if (equation[i] == '(')
+                {
+                    bracketcount++;
+                }
+                else if (equation[i] == ')')
+                {
+                    bracketcount--;
+                }
+                if (equation[i]==Character && bracketcount==0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
     }
 }
